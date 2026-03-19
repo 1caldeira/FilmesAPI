0e7da24aa89f1837304bf244e2479a11250a6fe6
1caldeira <g.caldeiraa1@gmail.com>
2026-03-19 18:56:16 -0300

    fix loginDTO

--- Data/DTOs/UsuarioDTO/LoginUsuarioDTO.cs
+++ Data/DTOs/UsuarioDTO/LoginUsuarioDTO.cs
@@ -5,7 +5,7 @@ namespace FilmesAPI.Data.DTO;
 public class LoginUsuarioDTO
 {
     [Required]
-    public string Username { get; set; }
+    public string Email { get; set; }
     [Required]
     public string Password { get; set; }
 }
